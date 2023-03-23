f6c9991247949f0571c854047074b6a0d5f0bd4b
mimuki <mimuki@proton.me>
2023-03-24 08:05:31 +1030

    Update installed programs

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-sudo apt install awesome keynav rofi kitty qutebrowser mate-polkit jq tlp lxappearance git gh kdeconnect
+sudo apt install awesome keynav rofi kitty qutebrowser mate-polkit-bin jq tlp lxappearance git gh kdeconnect vim sublime-text
