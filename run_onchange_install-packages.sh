fc45439ccb63179c68095906b56aa691d50922f0
mimuki <mimuki@proton.me>
2023-03-26 08:31:30 +1030

    Add nvm installer

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,2 +1,4 @@
 #!/bin/sh 
 sudo apt install awesome keynav rofi kitty qutebrowser mate-polkit-bin jq tlp lxappearance git gh kdeconnect vim sublime-text
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
