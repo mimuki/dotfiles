2810d6322f2dad56ddf1170bbd3acdd9b8956e73
mimuki <mimuki@proton.me>
2023-04-11 10:30:39 +0930

    Improve captions position on screen

--- dot_config/awesome/utilities.lua
+++ dot_config/awesome/utilities.lua
@@ -10,5 +10,5 @@ livecaptions = bling.module.scratchpad {
     floating = true,                                  -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
     geometry = {x=600, y=800}, -- The geometry in a floating state
     reapply = true,                                   -- Whether all those properties should be reapplied on every new opening of the scratchpad (MUST BE TRUE FOR ANIMATIONS)
-    dont_focus_before_close  = false,                 -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
+    dont_focus_before_close  = true,                 -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
 }
