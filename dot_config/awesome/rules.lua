b1ff0de068f99a9b645327a127984c3b6c9b1019
mimuki <60542090+mimuki@users.noreply.github.com>
2023-06-02 11:08:31 +0930

    Minor stuff

--- dot_config/awesome/rules.lua
+++ dot_config/awesome/rules.lua
@@ -43,7 +43,7 @@ awful.rules.rules = {
         "Wpa_gui",
         "veromix",
         "xtightvncviewer"
-    },
+      },
 
     -- Note that the name property shown in xprop might be set slightly after creation of the client
     -- and the name shown there might not match defined rules here.
@@ -76,6 +76,16 @@ awful.rules.rules = {
       floating = true,
       ontop = true,
       placement = awful.placement.top
+    }
+},
+
+
+{ rule = { class = "feh" },
+    properties = { 
+      --floating = true,
+      --ontop = true,
+      placement = awful.placement.centered,
+      maximized = true
     }
 }
 -- Set Firefox to always map on the tag named "2" on screen 1.
