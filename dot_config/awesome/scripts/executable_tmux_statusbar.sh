bd15c1f2cb365f2ec6e8ca18b7d448f9ff73d1b2
mimuki <60542090+mimuki@users.noreply.github.com>
2023-10-02 09:00:35 +1030

    improve tmux binds

---
+++ dot_config/awesome/scripts/executable_tmux_statusbar.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Make sure the statusbar updates
+# for the current directory
+tmux refresh-client -S
+
+# Show a different statusbar background
+# based on program
+#WINDOW=$(tmux list-windows | grep \* | awk '{print $2}')
+WINDOW=$(tmux lsw -F '#{window_name}#{window_active}'|sed -n 's|^\(.*\)1$|\1|p')
+if [[ $WINDOW = "xmpp" || $WINDOW = "csol" ]]
+then
+  tmux set-option status-bg "brightyellow"
+else
+  tmux set-option status-bg "#ffd477"
+fi
