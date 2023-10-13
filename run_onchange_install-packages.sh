09ecaec6f3e56bf88556f6df86d4fce63e42ec70
Mimuki <mimuki@proton.me>
2023-10-14 08:24:01 +1030

    Update run_onchange_install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -4,10 +4,3 @@
 # further down)
 sudo apt-get update
 sudo apt install keynav tlp git gh kdeconnect vim picom mpv scrot xclip
-# --- [ Curl ] ------------------------------------------------------- #
-# Some stuff needs special installation
-# nvm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-# vimplug
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
