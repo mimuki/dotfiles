590cc795f330aa7d4fb24c94a6e7abe1b7a52103
mimuki <60542090+mimuki@users.noreply.github.com>
2023-10-14 09:36:29 +1030

    move things around in a debiany way

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -3,4 +3,4 @@
 # (if a program has additional install stpes, consider putting it's install
 # further down)
 sudo apt-get update
-sudo apt install keynav tlp git gh kdeconnect vim picom mpv scrot xclip
+sudo apt install keynav tlp git gh kdeconnect vim mpv scrot xclip xterm tmux
