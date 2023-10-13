bb1f39664ab2a0eb7cf9b44426729e09db20b9d8
mimuki <60542090+mimuki@users.noreply.github.com>
2023-10-13 15:50:09 +1030

    todo make this good later

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,13 +1,9 @@
 #!/bin/sh 
-# --- [ Release Keys ] ------------------------------------------------------- #
-sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
-
 # --- [ Essential Software ] ------------------------------------------------------- #
 # (if a program has additional install stpes, consider putting it's install
 # further down)
 sudo apt-get update
-sudo apt install awesome keynav rofi mate-polkit-bin jq tlp lxappearance git gh kdeconnect vim picom mpv scrot xclip
+sudo apt install keynav tlp git gh kdeconnect vim picom mpv scrot xclip
 # --- [ Curl ] ------------------------------------------------------- #
 # Some stuff needs special installation
 # nvm
@@ -15,12 +11,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 # vimplug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# pyradio
-sudo apt -y install python3-rich python3-venv python3-pip
-curl -L https://raw.githubusercontent.com/coderholic/pyradio/master/pyradio/install.py -o install.py
-python3 install.py
-rm ~/install.py
-# Syncthing
-sudo apt -y install syncthing
-systemctl --user enable syncthing.service
-systemctl --user start syncthing.service
