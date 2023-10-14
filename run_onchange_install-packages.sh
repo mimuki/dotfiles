345544b94985602e7abb7db896de3205b49d1199
mimuki <60542090+mimuki@users.noreply.github.com>
2023-10-14 16:24:52 +1030

    AAAAAA I HATE IT HERE LET ME OUT

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,6 +1,19 @@
 #!/bin/sh 
-# --- [ Essential Software ] ------------------------------------------------------- #
-# (if a program has additional install stpes, consider putting it's install
-# further down)
 sudo apt-get update
-sudo apt install keynav tlp git gh kdeconnect vim mpv scrot xclip xterm tmux curl
+# Makes installing programs less verbose
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 keynav tlp git gh kdeconnect vim mpv scrot xclip xterm tmux curl rc
+
+# All the weird non-apt stuff
+if ! [ -x "$(command -v msync)" ]
+then
+  TEMP_FILE="$(mktemp)" &&
+  wget -O "$TEMP_FILE" 'https://github.com/Kansattica/msync/releases/download/v0.9.9.8/msync-0.9.9-Linux.deb' &&
+  sudo dpkg -i "$TEMP_FILE"
+  rm -f "$TEMP_FILE"
+  # todo: the rest of setup
+fi
+if ! [ -x "$(command -v fzf)" ]
+then
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+fi
