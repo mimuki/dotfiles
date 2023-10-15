18704478b4469d021f79b60ed3c1a7ddee186bed
mimuki <60542090+mimuki@users.noreply.github.com>
2023-10-15 12:44:34 +1030

    also this

--- run_onchange_after_github-repos.sh
+++ run_onchange_after_github-repos.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # TODO: i don't think this is the right way of checking if a dir exists
 if [ ! -d "~/Projects/halley" ]
+then
   git clone https://github.com/mimuki/halley.git ~/Projects/halley
 fi
 
