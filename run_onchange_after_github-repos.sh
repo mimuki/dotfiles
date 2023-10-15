c56d87703a57e0fa904aa1d426a12cc3665804f2
mimuki <60542090+mimuki@users.noreply.github.com>
2023-10-15 12:27:47 +1030

    aaaa

---
+++ run_onchange_after_github-repos.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# TODO: i don't think this is the right way of checking if a dir exists
+if [ ! -d "~/Projects/halley" ]
+  git clone https://github.com/mimuki/halley.git ~/Projects/halley
+fi
+
+
--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,7 +1,8 @@
 #!/bin/sh 
+#TODO: learn how arrays work
 sudo apt-get update
 # Makes installing programs less verbose
-sudo apt-get install -qq -o=Dpkg::Use-Pty=0 keynav tlp git gh kdeconnect vim mpv scrot xclip xterm tmux curl rc
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 keynav tlp git gh kdeconnect vim mpv scrot xclip xterm tmux curl rc gawk
 
 # All the weird non-apt stuff
 if ! [ -x "$(command -v msync)" ]
@@ -10,10 +11,18 @@ then
   wget -O "$TEMP_FILE" 'https://github.com/Kansattica/msync/releases/download/v0.9.9.8/msync-0.9.9-Linux.deb' &&
   sudo dpkg -i "$TEMP_FILE"
   rm -f "$TEMP_FILE"
-  # todo: the rest of setup
+  #ALERTS+=['Make sure you set up msync; the process is as follows:']
+  #ALERTS+=["  msync new -a username@example.com"]
+  #ALERTS+=['  Open the link in your browser and authorize it']
+  #ALERTS+=['  msync config auth_code <the authorization code from the site> --account username@example.com']
+  #ALERTS+=['  msync new -a username@example.com']
 fi
 if ! [ -x "$(command -v fzf)" ]
 then
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install --all
 fi
+
+echo All done!
+#printf '%s\n' "${ALERTS[@]}"
+
