ba99f1e4310cbf86180bac32cba9de69e6c46618
mimuki <60542090+mimuki@users.noreply.github.com>
2023-12-03 15:01:31 +1030

    center subtitles

--- dot_config/awesome/statusbar.lua
+++ dot_config/awesome/statusbar.lua
@@ -1,15 +1,16 @@
 function statusBar(s)
   s.mywibox:setup {
     layout = wibox.layout.align.horizontal,
+    -- ensures the middle widgets are centered
+    expand = "none",
     { -- Left widgets
       layout = wibox.layout.fixed.horizontal,
       mylauncher,
       s.mytaglist,
       s.currentProgram,
       s.mypromptbox,
-      subtitles,
     },
-      dummyMiddleWidget,
+      subtitles,
     { -- Right widgets
       layout = wibox.layout.fixed.horizontal,
       wibox.widget.systray(),
