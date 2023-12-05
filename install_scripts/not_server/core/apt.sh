f585de56a60709a8ed63876b151b335d3f99d081
mimuki <60542090+mimuki@users.noreply.github.com>
2023-12-05 17:45:10 +1030

    Fix a bunch of little things

--- install_scripts/not_server/core/apt.sh
+++ install_scripts/not_server/core/apt.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Things that aren't installed by default when you don't pick a gui option
+sudo apt-get install -qq -o=Dpkg::Use-Pty=0 xorg lightdm firefox-esr mate-polkit pavucontrol
+
 sudo apt-get install -qq -o=Dpkg::Use-Pty=0 awesome keynav rofi gammastep mpv scrot xclip xterm dex
 
 # literally need all of this for talon (because snixembed)
