c8ac460d5764c6526eba307c79c49ecd5b7482f5
mimuki <60542090+mimuki@users.noreply.github.com>
2023-12-10 19:51:43 +1030

    why didn't i do this with cron, lol

--- dot_tmux/plugins/rat_scripts/pk.sh
+++ dot_tmux/plugins/rat_scripts/pk.sh
@@ -1,9 +1,1 @@
-touch ~/Projects/sw/.count
-new=$(awk '(NR==1) { print ($1 + 1) % 60; }' ~/Projects/sw/.count)
-echo $new > ~/Projects/sw/.count
-if [[ $new == 0 ]]
-then
-  cd ~/Projects/sw
-  .venv/bin/python3.11 sw f tmux > ~/Projects/sw/.front
-fi
 cat ~/Projects/sw/.front
