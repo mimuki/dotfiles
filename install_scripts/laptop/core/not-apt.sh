44301ae60db8646407cd4f12f01ee83332a968bf
mimuki <60542090+mimuki@users.noreply.github.com>
2023-12-11 16:52:04 +1030

    configure tlp

--- install_scripts/laptop/core/not-apt.sh
+++ install_scripts/laptop/core/not-apt.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+# Laptop specific battery management 
+if [[ $USER == "mango" ]]; then
+  sudo cp ~/.local/share/chezmoi/manual_overrides/50-tlp-mango.conf /etc/tlp.d/
+fi
+# All laptops run tlp in this house
+sudo tlp start
