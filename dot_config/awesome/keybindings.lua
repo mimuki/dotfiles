50d6230813ea547af0b3cfe0f578961c7600d9ba
mimuki <60542090+mimuki@users.noreply.github.com>
2023-12-12 11:43:20 +1030

    Add focus mode

--- dot_config/awesome/keybindings.lua
+++ dot_config/awesome/keybindings.lua
@@ -95,6 +95,22 @@ awful.key(
   { modkey, "Shift" }, "space", function () awful.layout.inc(-1) end,
   { description = "Previous layout", group = "layout" }),
 
+awful.key(
+  { modkey }, "b", function ()
+    for s in screen do
+      -- Toggle statusbar
+      -- Sometimes this does the 
+      s.mywibox.visible = not s.mywibox.visible
+      -- Add margins on the sides if there's no statusbar
+      if s.mywibox.visible then
+        awful.screen.focused().padding = { left = "0", right = "0" }
+      else
+        awful.screen.focused().padding = { left = "90", right = "90" }
+      end
+    end
+  end,
+  { description = "Toggle focus mode", group = "awesome" }),
+
 -- adjust brightness
 awful.key(
   { }, "XF86MonBrightnessUp", function() os.execute("brightnessctl set 1%+") end),
@@ -138,7 +154,7 @@ awful.key(
 
 clientkeys = gears.table.join(
 awful.key(
-  { modkey }, "b", function (c)
+  { modkey, "Shift" }, "b", function (c)
     awful.titlebar.toggle(c, "top") end,
   { description = "toggle titlebar on focused client", group = "window management" }),
 
