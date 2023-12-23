fde2d8e8dd849cefa430f3fb3a4d8b6e0c1e217f
mimuki <60542090+mimuki@users.noreply.github.com>
2023-12-24 10:14:45 +1030

    Add navidrome

--- install_scripts/server/core/not-apt.sh
+++ install_scripts/server/core/not-apt.sh
@@ -27,3 +27,32 @@ if ! [ -x "$(command -v msync)" ]; then
 ║   --account you@example.com     ║\n\
 ║ $ msync new -a you@example.com  ║\n")
 fi
+
+if ! [ -x "$(command -v navidrome)" ]; then
+  sudo adduser --system --no-create-home --group navidrome
+  sudo install -d -o navidrome -g navidrome /opt/navidrome
+  sudo install -d -o navidrome -g navidrome /var/lib/navidrome
+
+  wget https://github.com/navidrome/navidrome/releases/download/v0.50.2/navidrome_0.50.2_linux_amd64.tar.gz -O Navidrome.tar.gz
+  sudo tar -xvzf Navidrome.tar.gz -C /opt/navidrome/
+  sudo chown -R navidrome:navidrome /opt/navidrome
+  sudo cp ~/.local/share/chezmoi/manual_overrides/navidrome.toml /var/lib/navidrome/navidrome.toml
+  sudo cp ~/.local/share/chezmoi/manual_overrides/navidrome.service /etc/systemd/system/
+
+  sudo systemctl daemon-reload
+  sudo systemctl start navidrome.service
+  sudo systemctl status navidrome.service
+
+  sudo systemctl enable navidrome.service
+
+  alerts+=("\
+║            navidrome            ║\n\
+╠═════════════════════════════════╣\n\
+║ navidrome was installed, now    ║\n\
+║ go to localhost:4533 and make   ║\n\
+║ an admin user.                  ║\n\
+║ If navidrome can't find any of  ║\n\
+║ your music, try running         ║\n\
+║ touch ~/Music/*/*/* so that the ║\n\
+║ library gets scanned again.     ║\n")
+fi
