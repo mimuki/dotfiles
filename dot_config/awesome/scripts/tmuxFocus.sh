0efd1e7d259c993b8618ac025479d2a4eea9f154
mimuki <60542090+mimuki@users.noreply.github.com>
2024-01-31 16:39:10 +1030

    improve tmux focus switching

--- dot_config/awesome/scripts/tmuxFocus.sh
+++ dot_config/awesome/scripts/tmuxFocus.sh
@@ -1,53 +1,16 @@
 #!/bin/bash
-#
-if [[ "$1" == '1' ]] ; then
-  if [[ $(tmux list-windows | wc -l) != "1" ]]; then
+
+if tmux show-option -vg prefix | grep a; then # the outer session is focused
+  if [[ "$1" == '1' ]] ; then
     tmux next-window
-    tmux send-keys F4
-    tmux set -qg status-bg brightwhite
-    tmux bind-key -n M-C-n \
-previous-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux bind-key -n C-S-Left \
-previous-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux bind-key -n M-C-o \
-next-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux bind-key -n C-S-Right \
-next-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux set -qg prefix C-a
   else
-    tmux send-keys F10
-    tmux send-keys F3
-
-    tmux set -qg status-bg white
-    tmux unbind -n M-C-n
-    tmux unbind -n C-S-Left
-    tmux unbind -n M-C-o
-    tmux unbind -n C-S-Right
-    tmux set -qg prefix C-b
-  fi
-else
-  if [[ $(tmux list-windows | wc -l) != "1" ]]; then
     tmux previous-window
-    tmux send-keys F4
-    tmux set -qg status-bg brightwhite
-    tmux bind-key -n M-C-n \
-previous-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux bind-key -n C-S-Left \
-previous-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux bind-key -n M-C-o \
-next-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux bind-key -n C-S-Right \
-next-window\; run-shell "~/.tmux/plugins/rat_scripts/statusbar.sh"
-    tmux set -qg prefix C-a
+  fi
+else # the inner session is focused
+  if [[ "$1" == '1' ]] ; then
+    tmux send-keys C-S-Right
   else
-    tmux send-keys F9
-    tmux send-keys F3
-
-    tmux set -qg status-bg white
-    tmux unbind -n M-C-n
-    tmux unbind -n C-S-Left
-    tmux unbind -n M-C-o
-    tmux unbind -n C-S-Right
-    tmux set -qg prefix C-b
+    tmux send-keys C-S-Left
   fi
 fi
+
